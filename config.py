ef5471138e3229bcd871344977bac2cafb9fe40c
Ben Lehman <ben.a.lehman@gmail.com>
2019-07-10 16:02:09 -0400

    Updated config to use env variables

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config(object):
+  SECRET_KEY = os.environ.get('SECRET_KEY') or "Ineedtochangethis"
+  HOST = ""
+  LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+  DEBUG = True
+  API_KEY = os.environ.get('API_KEY')
+  API_SEC = os.environ.get('API_SEC')
+  POSTGRES_PASS = os.environ.get('POSTGRES_PASS')
+  SHOPIFY_HOST = os.environ.get('SHOPIFY_HOST')
+
+
+  POSTGRES = {
+    'user': 'benlehman',
+    'pw': POSTGRES_PASS,
+    'db': 'mapping',
+    'host': 'localhost',
+    'port': '5432'
+  }
+
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+  SHOPIFY_CONFIG = {
+    'API_KEY': API_KEY,
+    'API_SEC': API_SEC,
+    'HOST': SHOPIFY_HOST,
+    'API_VERSION': "2019-07"
+  }
