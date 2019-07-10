10a5eb149a71a2ee54976cbfac3171b25c5ef819
Ben Lehman <ben.a.lehman@gmail.com>
2019-07-10 16:06:54 -0400

    Updated config handling for herkou deploy change

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,11 @@ from config import Config
 
 import os
 
-def create_app(test_config=None):
+def create_app(config_class=Config):
     # create and configure app
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+    app.config.from_object(config_class)
 
     if app.config['LOG_TO_STDOUT']:
         stream_handler = logging.StreamHandler()
