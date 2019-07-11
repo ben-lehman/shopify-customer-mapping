aca7829e8c1566ee9a855d8d70404409c5d90b67
Ben Lehman <ben.a.lehman@gmail.com>
2019-07-11 12:31:36 -0400

    Added an update route which get's new orders and pushes the info to the db

--- app/address.py
+++ app/address.py
@@ -3,10 +3,35 @@ from flask import (
 )
 from flask_cors import cross_origin
 from app.helpers.api import Orders
+from app.models import db
+from app.models import Customer
 
 import requests
 
 bp = Blueprint('address', __name__)
+
+
+@bp.route('/update', methods=('GET', 'POST'))
+def update():
+    order = Orders()
+    order.set_order_count()
+    order_count = order.count
+
+    order.set_order_locations()
+    order_locations = order.order_locations
+    print(type(order_locations))
+
+    for location in order_locations:
+        customer = Customer(name=location['name'],
+                            lat=location['latitude'],
+                            lng=location['longitude'],
+                            address1=location['address1'])
+        db.session.add(customer)
+        db.session.commit()
+
+    flash('Locations have been updated')
+    return render_template('orders/update.html')
+
 
 @bp.route('/api/orders', methods=('GET',))
 @cross_origin()
@@ -18,12 +43,8 @@ def orders():
     order.set_order_locations()
     order_locations = order.order_locations
 
-    order_coords = order.get_coordinates()
-
     return jsonify(order_locations)
 
 @bp.route('/map', methods=('GET',))
 def map():
-
-
     return render_template('orders/locations.html')
