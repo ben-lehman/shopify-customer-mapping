426144fcb86c8bb77edb00959ec04b5d007f0d3f
Ben Lehman <ben.a.lehman@gmail.com>
2019-07-12 14:18:35 -0400

    Last heroku postgres testing

--- config.py
+++ config.py
@@ -11,15 +11,15 @@ class Config(object):
   SHOPIFY_HOST = os.environ.get('SHOPIFY_HOST')
 
 
-  # POSTGRES = {
-  #   'user': 'benlehman',
-  #   'pw': POSTGRES_PASS,
-  #   'db': 'mapping',
-  #   'host': 'localhost',
-  #   'port': '5432'
-  # }
+  POSTGRES = {
+    'user': 'benlehman',
+    'pw': POSTGRES_PASS,
+    'db': 'mapping',
+    'host': 'localhost',
+    'port': '5432'
+  }
 
-  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
   SHOPIFY_CONFIG = {
